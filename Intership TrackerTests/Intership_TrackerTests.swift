1e36c072c3948e495a667bbf367b98b66657970e
Berat Zengin <devberatzengin@gmail.com>
2026-03-25 16:24:20 +0300

    Initial Commit

---
+++ Intership TrackerTests/Intership_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Intership_TrackerTests.swift
+//  Intership TrackerTests
+//
+//  Created by Berat Zengin on 25.03.2026.
+//
+
+import Testing
+@testable import Intership_Tracker
+
+struct Intership_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
